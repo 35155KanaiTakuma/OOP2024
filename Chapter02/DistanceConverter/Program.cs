f68ded384eb942d7d67fbb8ab059002164328875
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-04-18 14:19:10 +0900

    計算ロジックをクラスとして分類

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using distanceConverter;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -17,15 +18,17 @@ namespace DistanceConverter {
             }
         }
         private static void PrintFeetToMeterList(int start, int stop) {
+            FeetConverter fc = new FeetConverter();
             for (int feet = start; feet <= stop; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = fc.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
         }
 
         private static void PrintMeterToFeetList(int start, int stop) {
+            FeetConverter fc = new FeetConverter();
             for (int meter = start; meter <= stop; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = fc.FromMeter(meter);
                 Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
         }
@@ -37,7 +40,6 @@ namespace DistanceConverter {
         // メートルからフィートを求める
         static double MeterToFeet(int meter) {
             return meter / 0.3048;
-
         }
     }
 }
