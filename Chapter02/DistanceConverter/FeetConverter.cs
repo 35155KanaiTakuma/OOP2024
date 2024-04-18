d9d6432d5e32d0ec2c437a1201f28757b5079282
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-04-18 15:01:11 +0900

    距離換算プログラム完成

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace distanceConverter {
      public static class FeetConverter {
+        //private const double ratio = 0.3048;    // 定数
+        public static readonly double ratio = 0.3048;   // 定数(外部にも公開する場合)
+
         // フィートからメートルを求める
         public static double FromMeter(double meter) {
-            return meter * 0.3048;
+            return meter * ratio;
         }
 
         // メートルからフィートを求める
         public static double ToMeter(double feet) {
-            return feet / 0.3048;
+            return feet / ratio;
         }
     }
 }
