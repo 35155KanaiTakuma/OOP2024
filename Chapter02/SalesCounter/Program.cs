f8ed43051691de09ddec43bda12f9c7b7a05834a
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-04-24 13:22:40 +0900

    初版完成

--- Chapter02/SalesCounter/Program.cs
+++ Chapter02/SalesCounter/Program.cs
@@ -6,22 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SalesCounter {
-    internal class Program {
+    public class Program {
         static void Main(string[] args) {
-            List<Sale> shopdates = ReadSale(@"C:\Users\infosys\source\repos\OOP2024\Chapter02\SalesCounter\bin\Debug\date\Sales.csv");
 
-            // 戻り値のコレクションを一件ずつ出力する
-            foreach (Sale date in shopdates) {
-                Console .WriteLine("店名:{0} カテゴリー:{1} 売上:{2}",date.ShopName,date.ProductCategory,date.Amount);
-                
-                Console.WriteLine(date.ShopName + " " + date.ProductCategory + " " + date.Amount);
-
-                Console.WriteLine($"店名:{date.ShopName} カテゴリー:{date.ProductCategory} 売上:{date.Amount}");
+            SalesCounter sales = new SalesCounter(ReadSales("date/Sales.csv"));
+            Dictionary<string,int> amountPerStore = sales.GetPerSStorSales();
+            foreach(KeyValuePair<string,int> obj in amountPerStore) {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
         }
 
         // 売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSale(string filePath) {
+        static List<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) 
