18337ce87ad1abd6adf9fcd26fa33e97d8faae97
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-04-24 14:08:49 +0900

    メソッドの移動

--- Chapter02/SalesCounter/Program.cs
+++ Chapter02/SalesCounter/Program.cs
@@ -9,28 +9,11 @@ namespace SalesCounter {
     public class Program {
         static void Main(string[] args) {
 
-            SalesCounter sales = new SalesCounter(ReadSales("date/Sales.csv"));
+            SalesCounter sales = new SalesCounter("date/Sales.csv");
             Dictionary<string,int> amountPerStore = sales.GetPerSStorSales();
             foreach(KeyValuePair<string,int> obj in amountPerStore) {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
-        }
-
-        // 売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {
-            List<Sale> sales = new List<Sale>();
-            string[] lines = File.ReadAllLines(filePath);
-            foreach (string line in lines) 
-            {
-                string[] items = line.Split(',');
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);
-            }
-            return sales;
         }
     }
 }
