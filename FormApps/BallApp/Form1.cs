83eb4013dcebc7699a3e2e345ae5073f3933ea35
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-05-08 13:40:05 +0900

    マウスのクリックした座標にボールが出現

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -1,48 +1,39 @@
 namespace BallApp {
     public partial class Form1 : Form {
-        private double posX;    // X座標
-        private double posY;    // Y座標
-        private double moveX;   // 移動量（X方向）
-        private double moveY;   // 移動量（Y方向）
-
-
-
+        SoccerBall soccerBall;
+        PictureBox pb;
         // コンストラクタ
         public Form1() {
             InitializeComponent();
-
-            moveX = moveY = 4; 
         }
 
         private void button1_Click(object sender, EventArgs e) {
-
         }
 
         // フォームが最初にロードされるとき一度だけ実行される
         private void Form1_Load(object sender, EventArgs e) {
-            this.BackColor = Color.GreenYellow;
-            timer1.Start();
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            // 現在位置を表示
-            this.Text = pbBall.Location.ToString();
+            soccerBall.Move();
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+        }
 
+        private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
-            if(pbBall.Location.X > 750 || pbBall.Location.X < 0) {
-                // 移動量の符号を反転
-                moveX = -moveX;
-            }
+            pb = new PictureBox();   // 画像を表示するコントロール
+            pb.Size = new Size(50,50);
 
-            if (pbBall.Location.Y > 500 || pbBall.Location.Y < 0) {
-                // 移動量の符号を反転
-                moveY = -moveY;
-            }
+            soccerBall = new SoccerBall(e.X, e.Y);
 
-            posX += moveX;
-            posY += moveY;
+            pb.Image =soccerBall.Image;
+            pb.Location = new Point((int)soccerBall.PosX,(int)soccerBall.PosY);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.Parent = this;
 
-            pbBall.Location = new Point((int)posX, (int)posY);
+            timer1.Start();
+
         }
     }
 }
