b9f25a72881faee5477979790a3380029d1cebae
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-05-15 15:57:52 +0900

    演習問題２作成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
@@ -11,40 +12,46 @@ namespace Exercise01 {
             var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
 
             // 3.1.1
-            Exercise1_1(numbers,);
-            foreach(var n in numbers) {
-                if (n / 8 == 0)
-            }
+            Exercise1_1(numbers);
             Console.WriteLine("-----");
 
             // 3.1.2
             Exercise1_2(numbers);
             Console.WriteLine("-----");
 
+
             // 3.1.3
             Exercise1_3(numbers);
             Console.WriteLine("-----");
 
+
+
             // 3.1.4
             Exercise1_4(numbers);
+            Console.WriteLine("-----");
         }
 
-        private static void Exercise1_1(List<int> numbers, int n) {
-            int num = 0;
-
-            }            
+        private static void Exercise1_1(List<int> numbers) {
+            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            if (exist)
+                Console.WriteLine("---------");
+            else
+                Console.WriteLine("-----");
         }
+
 
         private static void Exercise1_2(List<int> numbers) {
-        
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
         }
 
         private static void Exercise1_3(List<int> numbers) {
-        
+            numbers.Where(n => 50 <= n).ToList()
+                                       .ForEach(n => Console.WriteLine(n));
         }
 
         private static void Exercise1_4(List<int> numbers) {
-        
+            numbers.Select(n => n * 2).ToList().ForEach(n => Console.WriteLine(n));
         }
     }
 }
+
