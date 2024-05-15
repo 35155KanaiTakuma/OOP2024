788c01738c257e48efc24c42ec2fa14c9054840c
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-05-15 15:02:17 +0900

    問題演習

---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1_1(numbers,);
+            foreach(var n in numbers) {
+                if (n / 8 == 0)
+            }
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+        }
+
+        private static void Exercise1_1(List<int> numbers, int n) {
+            int num = 0;
+
+            }            
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+        
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+        
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+        
+        }
+    }
+}
