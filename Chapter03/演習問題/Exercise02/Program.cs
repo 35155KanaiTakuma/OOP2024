dae0a83896b905667a511f6a5eaa323986a9489f
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-05-22 10:53:43 +0900

    3.2.1終了

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -10,22 +10,44 @@ namespace Exercise02 {
             var names = new List<string> {
      "Tokyo", "New Delhi", "Bangkok", "London",
                 "Paris", "Berlin", "Canberra", "Hong Kong",
-};
+            };
+
+            Console.WriteLine("***** 3.2.1 *****");
             Exercise2_1(names);
             Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.1 *****");
             Exercise2_2(names);
             Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.1 *****");
             Exercise2_3(names);
+            Console.WriteLine("***** 3.2.1 *****");
+
+            Console.WriteLine("***** 3.2.1 *****");
             Console.WriteLine();
             Exercise2_4(names);
         }
 
         private static void Exercise2_1(List<string> names) {
-        
+
+            Console.WriteLine("都市名を入力。空行で終了");
+
+            do {
+                var line = Console.ReadLine();
+                // 入力処理
+
+                if (string.IsNullOrEmpty(line))
+                    break;  // 空行だったら抜ける
+
+                int index = names.FindIndex(s => s == line );
+                Console.WriteLine(index);
+            }while(true);
+
         }
 
         private static void Exercise2_2(List<string> names) {
-        
+        names.Count();
         }
 
         private static void Exercise2_3(List<string> names) {
