76986c8c8aac35bd2eace205dd67aeb7bfce14c6
KanaiTakuma <166583906+35155KanaiTakuma@users.noreply.github.com>
2024-05-30 13:24:45 +0900

    問題5.1終了

---
+++ chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+        }
+
+        private static void Exercise3_1(string text) {
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            if (string.Compare(str1,str2,true) == 0) //ignoreCaseを使ってもok
+                Console.WriteLine("文字列が等しい");
+             else 
+                Console.WriteLine("文字列が等しくない");
+        }
+
+        private static void Exercise3_2(string text) {
+            var num = Console.ReadLine();
+            
+
+            
+        }
+
+        private static void Exercise3_3(string text) {
+            
+        }
+
+        private static void Exercise3_4(string text) {
+            
+        }
+
+        private static void Exercise3_5(string text) {
+            
+        }
+    }
+}
